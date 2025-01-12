3966339dac559bb2579133a9c9d823f37cdb3902
karan-raval <karanraval424@gmail.com>
2025-01-12 12:31:10 +0530

    Header of Herbal me

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import './Home/home.css'
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
