0aeee9f1dd06561c2a135dee5a31e7c7a3f38346
karan-raval <karanraval424@gmail.com>
2025-01-15 18:01:33 +0530

    Herbal me

---
+++ Frontend/src/About-US/AboutUs.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+const AboutUs = () => {
+  return (
+    <>
+    <Header/>
+    <main class="main-content js-focus-hidden" id="MainContent" role="main" tabindex="-1">
+      <div class="page-width">
+  <div class="grid">
+    <div class="grid__item medium-up--five-sixths medium-up--push-one-twelfth">
+      <div class="section-header text-center">
+        <h1>About Us</h1>
+      </div>
+
+      <div class="rte">
+        <h2 style={{ textAlign: 'center' }}>WHO WE ARE</h2>
+<div style={{ textAlign: 'center' }}></div>
+<div style={{ textAlign: 'center' }}>We are manufacturers and exporters of truly organic and natural body care products since 2014. The products made by Herbal Me are&nbsp;free from synthetic chemicals and comprise only of herbal botanicals. Our products&nbsp;been&nbsp;certified by&nbsp;<strong>ECOCERT&nbsp;(France) in accordance&nbsp;with COSMOS v3 standards</strong>&nbsp;which is a globally recognized certification body to develop standards for truly natural and organic cosmetics, worldwide. The&nbsp;products manufactured by us are&nbsp;<strong>Halal</strong> compliant. They are not tested on animals and are approved by&nbsp;<strong>PETA</strong>&nbsp;India as <strong>Cruelty Free and Vegan</strong>. Herbal Me is an&nbsp;ISO 9001:2015 and GMP&nbsp;certified Enterprise adhering to the highest standards for <strong>Good Manufacturing Practices</strong>.</div>
+<div style={{ textAlign: 'center' }}></div>
+<div style={{ textAlign: 'center' }}></div>
+<div style={{ textAlign: 'center' }}></div>
+<h2 style={{ textAlign: 'center' }}>FOUNDER PROFILE</h2>
+<p style={{ textAlign: 'center' }}>Bindiya Rawal, an under grad from Presentation Convent School followed by graduation from Shri Ram College of Commerce (Delhi University) has led a life on her own terms. Coming from a business-oriented academic background, she has managed to apply her skills on the professional front effectively. Bindiya started working at the young age of 21, where she was in the business of exporting sheet glass. This was then predominantly a male domain, but she became one of the very few women who undertook the work with equal tact and finesse, if not better. Later on she added a travel agency to her profile.This is when she started following her passion for travel. By mixing her love for travelling with her skill of handling business, she created nothing but phenomenal success.</p>
+<h2 style={{ textAlign: 'center' }}>HERBAL ME MISSION</h2>
+<div style={{ textAlign: 'center' }}><strong>To reconnect mankind with nature</strong></div>
+<div style={{ textAlign: 'center' }}><strong></strong></div>
+<div style={{ textAlign: 'center' }}></div>
+<div style={{ textAlign: 'center' }}>Our endeavor is to uncover the most exotic secrets of nature and bring them to you through our certified organic and natural products.&nbsp;We aim to totally eliminate synthetic chemicals from our daily body-care regime.&nbsp;Our customers are more than happy with our products and this is a constant motivation for us to do better. We are now channelizing this positive energy into innovation of newer and diverse products.&nbsp;</div>
+<div style={{ textAlign: 'center' }}></div>
+<div style={{ textAlign: 'center' }}></div>
+<div style={{ textAlign: 'center' }}>Mankind has to understand that although the chemical based cosmetics deliver faster, they take away a bit of us every time we use them. It’s time to get in touch with Mother Nature, it’s time to get Herbal Me!</div>
+<div style={{ textAlign: 'center' }}></div>
+<div style={{ textAlign: 'center' }}></div>
+<h2 style={{ textAlign: 'center' }}>VISION</h2>
+<p style={{ textAlign: 'center' }}>What started off as a day-to-day kitchen project is now a well established enterprise and growing rapidly. In a very short span Herbal Me has made a very strong foothold in&nbsp;supplying cosmetics to North American,&nbsp;European, South Asian and Middle Eastern markets. We have an ever expanding audience, which motivates us to do better, innovate and diversify, into other categories and products. We are very keen to offer our own products, which have been made with so much passion and hard work, to the whole world and thus take our traditional Indian Ayurvedic knowledge at a global level. Through Herbal Me, Bindiya dreams of restoring people's&nbsp;faith in the power of mother nature, by delivering the purest part of it in the form of her products.</p>
+<h2 contenteditable="true" data-elementor-setting-key="title" data-pen-placeholder="Type Here..." style={{ textAlign: 'center' }}>HOW IT ALL STARTED</h2>
+<p style={{ textAlign: 'center' }}>A great believer in the power of nature, Bindiya always had a keen interest in Ayurveda and natural remedies. She was always experimenting in the kitchen making face packs and oils with natural ingredients, easily available at home. She would buy things from the market, add an exotic ingredient and come up with a totally new product. Till date she is experimenting with new recipes, in search of natural solutions. When results of her experiments started to show on her skin and hair, people got inquisitive to know about her body care regime. As a result, she started giving out samples to her friends and family. This created quite a stir and she built the first set of a dedicated audience. This was also when she decided to take it a notch higher and build it into a full-fledged professionally managed enterprise. A glorious result of that is Herbal Me by H.M. Enterprises.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default AboutUs
